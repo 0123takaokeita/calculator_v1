606dcb6f3dd026ee55fcbd3680ae0a862582513c
Keita Takao <keita.takao.1112@gmail.com>
2021-08-06 22:59:32 +0900

    get input form

--- index.js
+++ index.js
@@ -1,49 +1,24 @@
+// フォームDOM取得
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
 
-  // アラートでテストインプットの出力
-  document.getElementById('testInput').addEventListener('click',function(){
-    
-    let operand1 = document.getElementById('input1').value;
-    let operand2 = document.getElementById('input2').value;
-    alert(`ope1: ${operand1}  ope2: ${operand2}`);
-  })
-  
-  //加算
-  document.getElementById('additionBtn').addEventListener('click',function(){
-    let operand1 = document.getElementById('input1').value;
-    let operand2 = document.getElementById('input2').value;
-    let answer   = document.getElementById('answer');
-    console.log(typeof operand1);
-    // console.log(Number(operand2));
-    answer.value = Number(operand1) + Number(operand2);
-    
-  }) 
-  // 割り算
-  document.getElementById('divisionBtn').addEventListener('click',function(){
-    let operand1 = document.getElementById('input1').value;
-    let operand2 = document.getElementById('input2').value;
-    let answer   = document.getElementById('answer');
-    console.log(typeof operand1);
+// 値の初期化
+let op1 = 0;
+let op2 = 0;
 
-    answer.value = operand1 / operand2;
-    
-  }) 
-  // 掛算
-  document.getElementById('multiplicationBtn').addEventListener('click',function(){
-    let operand1 = document.getElementById('input1').value;
-    let operand2 = document.getElementById('input2').value;
-    let answer   = document.getElementById('answer');
-    console.log(typeof operand1);
-    answer.value = operand1 * operand2;
-    
-  }) 
-  // 引き算
-  document.getElementById('subtractionBtn').addEventListener('click',function(){
-    let operand1 = document.getElementById('input1').value;
-    let operand2 = document.getElementById('input2').value;
-    let answer   = document.getElementById('answer');
-    console.log(typeof operand1);
+// 入力値の取得
+input1.addEventListener('change', function () {
+  op1 = input1.value;
+})
+input2.addEventListener('change', function () {
+  op2 = input2.value;
+})
 
-    answer.value = operand1 - operand2;
-    
-  }) 
-  
+
+// 入力値をアラートで確認。
+const testInput = document.getElementById('testInput');
+testInput.addEventListener('click', function () {
+  alert(`operand1: ${op1} operand2: ${op2}`);
+});
+
+
