cf36c1950539665ed1abec6a0556b316dff469de
Keita Takao <keita.takao.1112@gmail.com>
2021-08-06 23:14:03 +0900

    add addition

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // フォームDOM取得
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
+const output = document.getElementById('answer');
 
 // 値の初期化
 let op1 = 0;
@@ -8,10 +9,10 @@ let op2 = 0;
 
 // 入力値の取得
 input1.addEventListener('change', function () {
-  op1 = input1.value;
+  op1 = parseInt(input1.value);
 })
 input2.addEventListener('change', function () {
-  op2 = input2.value;
+  op2 = parseInt(input2.value);
 })
 
 
@@ -19,6 +20,13 @@ input2.addEventListener('change', function () {
 const testInput = document.getElementById('testInput');
 testInput.addEventListener('click', function () {
   alert(`operand1: ${op1} operand2: ${op2}`);
+});
+
+
+// 足し算を行う。
+const plus = document.getElementById('additionBtn')
+plus.addEventListener('click',function(){
+  output.value = op1 + op2;
 });
 
 
